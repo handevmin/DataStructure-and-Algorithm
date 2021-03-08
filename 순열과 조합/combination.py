3e9986bd60b968ec130fd0490c7b71beea66b8ce
handevmin <77570421+handevmin@users.noreply.github.com>
2021-03-08 23:19:33 +0900

    update combination.py

--- 순열과 조합/combination.py
+++ 순열과 조합/combination.py
@@ -1,11 +1,21 @@
 nums = [1, 0, -1, 0, -2, 2]
-target = 0
+k = 3
 result = []
+index = 0
 
-def combination(nums, target):
-    if len(result) == 1:
-        return nums[0]
-    else:
-        combination(nums[1:], target)
 
-print(combination(nums, target))
+def combination(lst, n):
+    ret = []
+    if n > len(lst): return ret
+
+    if n == 1:
+        for i in lst:
+            ret.append([i])
+    elif n > 1:
+        for i in range(len(lst) - n + 1):
+            for temp in combination(lst[i + 1:], n - 1):
+                ret.append([lst[i]] + temp)
+
+    return ret
+
+print(combination(nums, k))
