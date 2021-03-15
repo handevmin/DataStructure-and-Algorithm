c2f1ef2c07625d3b18a253f471ce1b1247dbd8ad
handevmin <77570421+handevmin@users.noreply.github.com>
2021-03-15 23:44:29 +0900

    add 1543_문서 검색.py

---
+++ 1543_문서 검색.py
@@ -0,0 +1,18 @@
+import sys
+
+document = sys.stdin.readline().rstrip()
+target = sys.stdin.readline().rstrip()
+count =0
+skip = 0
+for i in range(len(document)):
+    if skip == 0 and document[i:len(target)+i] == target:
+        count+=1
+        skip = len(target)
+    else:
+        if skip ==0:
+            continue
+        else:
+            skip-=1
+print(count)
+
+
