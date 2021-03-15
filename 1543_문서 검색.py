7877565f8ac9c2f3a82d2c43203e21c26f7afb44
handevmin <77570421+handevmin@users.noreply.github.com>
2021-03-16 00:11:14 +0900

    update 1543_문서 검색.py

--- 1543_문서 검색.py
+++ 1543_문서 검색.py
@@ -7,7 +7,8 @@ skip = 0
 for i in range(len(document)):
     if skip == 0 and document[i:len(target)+i] == target:
         count+=1
-        skip = len(target)
+        if len(target)!=1:
+            skip = len(target)-1
     else:
         if skip ==0:
             continue
