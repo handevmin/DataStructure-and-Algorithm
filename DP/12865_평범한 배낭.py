233930cc57603287f89dc2dbad6deb4bf89996f0
handevmin <77570421+handevmin@users.noreply.github.com>
2021-03-23 19:38:22 +0900

    update 12865_평범한 배낭.py

--- DP/12865_평범한 배낭.py
+++ DP/12865_평범한 배낭.py
@@ -1,7 +1,7 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
-<<<<<<< HEAD
+
 dp = [[0] * (K) for i in range(N)]
 print(dp)
 for i in range(N):
@@ -13,10 +13,3 @@ for i in range(N):
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-W]+V)
 print(dp)
 print(dp[N-1][K-1])
-=======
-item = []
-for i in range(N):
-    item.append(list(map(int, sys.stdin.readline().split())))
-
-print(item)
->>>>>>> origin/master
